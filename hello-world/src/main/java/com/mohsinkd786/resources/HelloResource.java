d35de25a480d789d523a96ef3275602367034589
techmomo <mkhursheed@spireon.com>
2021-10-05 12:08:53 +0530

    Post

--- hello-world/src/main/java/com/mohsinkd786/resources/HelloResource.java
+++ hello-world/src/main/java/com/mohsinkd786/resources/HelloResource.java
@@ -1,7 +1,7 @@
 package com.mohsinkd786.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloResource {
@@ -10,9 +10,23 @@ public class HelloResource {
     public String sayHello(){
         return "Hello Resource";
     }
+
+    @GetMapping("/message/{str}")
+    public String getMessage(@PathVariable("str") String msg){
+        return "Message :"+msg;
+    }
+
+    @GetMapping(value = "/params",produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getParams(@RequestParam String msg,@RequestParam boolean status){
+       return status? msg: "No Message";
+    }
+
 }
 
 /*
 * Spring Rest : GET / POST / PUT / DELETE / PATCH
+*
+* PathVariables - they are ideally the mandatory params
+* Request params - query params, ideally the optional ones
 *
 * */
